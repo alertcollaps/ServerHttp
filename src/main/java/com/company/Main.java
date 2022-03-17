bde081987735190fd47834d97f2823c546bbda62
borisov.aleksey18 <borisov.aleksey18@wb.ru>
2022-03-17 13:45:25 +0300

    Heroku

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
         try {
             System.out.println("Start");
             System.out.println(Integer.parseInt(System.getenv("PORT")));
-            HttpServer server = HttpServer.create(new InetSocketAddress("localhost", Integer.parseInt(System.getenv("PORT"))), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", Integer.parseInt(System.getenv("PORT"))), 0);
             server.createContext("/", new HandlerHttp());
             server.start();
         } catch (IOException e) {
